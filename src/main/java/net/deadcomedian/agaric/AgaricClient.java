0fc2a9e1aba2aa3513a9551876527c322ccaa177
The-Dead-Comedian <113227369+The-Dead-Comedian@users.noreply.github.com>
2023-12-29 21:19:38 +0200

    Initial commit

---
+++ src/main/java/net/deadcomedian/agaric/AgaricClient.java
@@ -0,0 +1,10 @@
+package net.deadcomedian.agaric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AgaricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
